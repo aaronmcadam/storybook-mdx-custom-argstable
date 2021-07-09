b1383895210ece46ffdef9e8f4042c19cdc16cbb
Aaron McAdam <aaron@aaronmcadam.com>
2021-07-09 18:38:23 +0100

    Demonstrate issue with ArgsTable in MDX

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+module.exports = {
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "@emotion/core": "@emotion/react",
+          "emotion-theming": "@emotion/react",
+        },
+      },
+    };
+  },
+};
---
+++ .storybook/preview.js
@@ -0,0 +1,18 @@
+import { ChakraProvider } from "@chakra-ui/react";
+
+export const decorators = [
+  (Story) => (
+    <ChakraProvider>
+      <Story />
+    </ChakraProvider>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      date: /Date$/,
+    },
+  },
+};
